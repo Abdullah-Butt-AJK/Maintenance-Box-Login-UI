3bf93ea8f3185568eda00989ad3dd03337305789
Abdullah Butt <abdullah.butt.dev3@gmail.com>
2026-06-12 21:23:45 +0500

    Add fix to Render Flex.

--- lib/main.dart
+++ lib/main.dart
@@ -15,165 +15,177 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           backgroundColor: Colors.white,
           body: SafeArea(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                //Main Logo
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image(
-                        height: 60,
-                        width: 60,
-                        image: AssetImage('images/logo.png')),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Maintenance',
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontFamily: 'Rubik Medium',
-                              color: Color(0xff2D3142)),
-                        ),
-                        Text(
-                          'Box',
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontFamily: 'Rubik Medium',
-                              color: Color(0xffF9703B)),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                //Login Text
-                const Center(
-                    child: Text(
-                  'Login',
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontFamily: 'Rubik Medium',
-                      color: Color(0xff2D3142)),
-                )),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Center(
-                    child: Text(
-                  'Log in if you already have an account',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontFamily: 'Rubik Regular',
-                      color: Color(0xff4C5980)),
-                )),
-                //Text Field Email
-                const SizedBox(
-                  height: 50,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        hintText: 'Email',
-                        fillColor: Color(0xffF8F9FA),
-                        filled: true,
-                        prefixIcon: const Icon(
-                          Icons.email_outlined,
-                          color: Color(0xff323F4B),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black26),
-                          borderRadius: BorderRadius.circular(12),
-                        )),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 80,
                   ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                //Text Field Password
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        hintText: 'Password',
-                        fillColor: const Color(0xffF8F9FA),
-                        filled: true,
-                        prefixIcon: const Icon(
-                          Icons.lock_outline_rounded,
-                          color: Color(0xff323F4B),
-                        ),
-                        suffixIcon: const Icon(
-                          Icons.visibility_off_outlined,
-                          color: Color(0xff323F4B),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black26),
-                          borderRadius: BorderRadius.circular(12),
-                        )),
-                  ),
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                //Login Button
-                Container(
-                  height: 50,
-                  width: 200,
-                  decoration: BoxDecoration(
-                      color: Color(0xffF9703B),
-                      borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xffF9703B).withValues(alpha: 0.8),
-                      blurRadius: 8,
-                      offset: Offset(0, 5),
-                    )
-                  ]
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'LOG IN',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontFamily: 'Rubik Regular',
-                        color: Colors.white,
+                  //Main Logo
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image(
+                          height: 60,
+                          width: 60,
+                          image: AssetImage('images/logo.png')),
+                      SizedBox(
+                        width: 10,
                       ),
-                    ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Maintenance',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontFamily: 'Rubik Medium',
+                                color: Color(0xff2D3142)),
+                          ),
+                          Text(
+                            'Box',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontFamily: 'Rubik Medium',
+                                color: Color(0xffF9703B)),
+                          )
+                        ],
+                      )
+                    ],
                   ),
-                ),
-                const SizedBox(height: 10),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account ?',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  //Login Text
+                  const Center(
+                      child: Text(
+                    'Login',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'Rubik Medium',
+                        color: Color(0xff2D3142)),
+                  )),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Center(
+                      child: Text(
+                    'Log in if you already have an account',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
                         fontSize: 16,
                         fontFamily: 'Rubik Regular',
-                        color: Color(0xff4C5980),
+                        color: Color(0xff4C5980)),
+                  )),
+                  //Text Field Email
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Email',
+                          fillColor: Color(0xffF8F9FA),
+                          filled: true,
+                          prefixIcon: const Icon(
+                            Icons.email_outlined,
+                            color: Color(0xff323F4B),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black26, width: 2),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
                       ),
                     ),
-                    Text('Sign Up',
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  //Text Field Password
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Password',
+                          fillColor: const Color(0xffF8F9FA),
+                          filled: true,
+                          prefixIcon: const Icon(
+                            Icons.lock_outline_rounded,
+                            color: Color(0xff323F4B),
+                          ),
+                          suffixIcon: const Icon(
+                            Icons.visibility_off_outlined,
+                            color: Color(0xff323F4B),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black26, width: 2),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  //Login Button
+                  Container(
+                    height: 50,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        color: Color(0xffF9703B),
+                        borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xffF9703B).withValues(alpha: 0.8),
+                        blurRadius: 8,
+                        offset: Offset(0, 5),
+                      )
+                    ]
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'LOG IN',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'Rubik Regular',
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Don\'t have an account ?',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
                           fontSize: 16,
-                          fontFamily: 'Rubik Medium',
-                          color: Color(0xffF9703B),
-                        ))
-                  ],
-                ),
-              ],
+                          fontFamily: 'Rubik Regular',
+                          color: Color(0xff4C5980),
+                        ),
+                      ),
+                      Text(' Sign Up',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'Rubik Medium',
+                            color: Color(0xffF9703B),
+                          ))
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ));
