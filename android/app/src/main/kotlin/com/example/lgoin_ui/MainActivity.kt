9761a8c856d9f7bda57f1696683cd3d61c7cb885
Abdullah Butt <abdullah.butt.dev3@gmail.com>
2026-06-12 17:52:24 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lgoin_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lgoin_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: Column(
+              children: [
+                Center(
+                    child: Text(
+                  'Login',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: 'Rubik Medium',
+                      color: Color(0xff2D3142)),
+                )),
+                SizedBox(height: 14,),
+                Center(
+                    child: Text(
+                      'Log in if you already have an account',
+                        textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Rubik Regular',
+                          color: Color(0xff4C5980)),
+                    ))
+              ],
+            ),
+          ),
+        ));
+  }
+}
